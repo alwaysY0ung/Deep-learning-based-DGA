722d5310b4ae1fc4a4a53f910d08fd7923bc23ce
정채리 <chaerry502@sookmyung.ac.kr>
2026-01-07 00:49:51 +0900

    [#4] sep 추가

--- preprocessing.py
+++ preprocessing.py
@@ -15,12 +15,13 @@ class SpecialIDs:
 def mtp_dataset(inputs, special_ids, max_len, mask_ratio=0.15, ignore_idx=-100) :
     labels = np.full(max_len, ignore_idx, dtype=np.int64)
     non_padding_indices = np.where(inputs != special_ids.pad_id)[0]
+    non_padding_indices = non_padding_indices[:-1]
 
     if len(non_padding_indices) <= 1:
         return inputs, labels
     
     # 마스크할 토큰 개수(최소 1개)
-    num_mask = max(1, int(len(non_padding_indices) * mask_ratio)) 
+    num_mask = max(1, int(len(non_padding_indices) * mask_ratio))
 
     masked_indices = random.sample(non_padding_indices.tolist(), num_mask)
 
@@ -37,6 +38,7 @@ def tpp_dataset(inputs, special_ids, ignore_idx=-100) :
     labels = np.copy(inputs)
     labels[inputs == special_ids.pad_id] = ignore_idx
     non_padding_indices = np.where(inputs != special_ids.pad_id)[0]
+    non_padding_indices = non_padding_indices[:-1]
 
     if len(non_padding_indices) <= 1:
         return inputs, labels
@@ -50,11 +52,13 @@ def tpp_dataset(inputs, special_ids, ignore_idx=-100) :
     for i, original_pos in enumerate(shuffled_indices):
         new_pos = permuted_indices[i]
         shuffled_inputs[new_pos] = inputs[original_pos]
+
     return shuffled_inputs, labels
 
 
-def tov_dataset(inputs, special_ids, max_len, shuffle_prob=0.5, tov_norm='pool') :
+def tov_dataset(inputs, special_ids, max_len, shuffle_prob=0.5, tov_norm='cls') :
     non_padding_indices = np.where(inputs != special_ids.pad_id)[0]
+    non_padding_indices = non_padding_indices[:-1]
     processed_inputs = np.copy(inputs)
 
     if len(non_padding_indices) <= 1:
@@ -74,14 +78,16 @@ def tov_dataset(inputs, special_ids, max_len, shuffle_prob=0.5, tov_norm='pool')
 
     pure_tokens = processed_inputs[non_padding_indices].tolist()
 
-    if len(pure_tokens) > max_len - 1:
-        pure_tokens = pure_tokens[:max_len - 1]
+    if len(pure_tokens) > max_len - 2:
+        pure_tokens = pure_tokens[:max_len - 2]
 
     if tov_norm == 'cls' :
         ids = [special_ids.cls_id]
         ids.extend(pure_tokens)
     elif tov_norm == 'pool' :
         ids = pure_tokens
+
+    ids.append(special_ids.sep_id)
 
     if len(ids) < max_len:
         ids += [special_ids.pad_id] * (max_len - len(ids))
@@ -91,7 +97,7 @@ def tov_dataset(inputs, special_ids, max_len, shuffle_prob=0.5, tov_norm='pool')
 
 class SubTaskDataset(Dataset) :
     def __init__(self, df, domain_col='domain', label_col='label', max_len=77, mask_ratio=0.15, ignore_idx=-100, shuffle_prob = 0.5,
-                tokenizer=None, special_ids=SpecialIDs, type='char', tov_norm='pool'):
+                tokenizer=None, special_ids=SpecialIDs, type='char', tov_norm='cls'):
         self.df = df
         self.domain_col = domain_col
         self.label_col = label_col
@@ -104,6 +110,7 @@ class SubTaskDataset(Dataset) :
         self.unk_idx = special_ids.unk_id
         self.mask_idx = special_ids.mask_id
         self.cls_idx = special_ids.cls_id
+        self.sep_idx = special_ids.sep_id
         self.type = type
         self.tov_norm = tov_norm
         if self.type == 'char' :
@@ -126,9 +133,11 @@ class SubTaskDataset(Dataset) :
             token_indices = [self.char2id.get(c, self.unk_idx) for c in domain]
 
         # zero padding
-        if len(token_indices) > self.max_len:
-            token_indices = token_indices[:self.max_len]
+        if len(token_indices) > self.max_len - 1:
+            token_indices = token_indices[:self.max_len - 1]
+            token_indices.append(self.sep_idx)
         else:
+            token_indices.append(self.sep_idx)
             token_indices += [self.pad_idx] * (self.max_len - len(token_indices))
         return np.array(token_indices, dtype=np.int64)
 
@@ -168,7 +177,7 @@ class SubTaskDataset(Dataset) :
   
     
 class FineTuningDataset(Dataset) :
-    def __init__(self, df, domain_col='domain', label_col='label', special_ids=SpecialIDs, max_len_t=30, max_len_c=77, tokenizer=None, clf_norm='pool'):
+    def __init__(self, df, domain_col='domain', label_col='label', special_ids=SpecialIDs, max_len_t=30, max_len_c=77, tokenizer=None, clf_norm='cls'):
         self.df = df
         self.domain_col = domain_col
         self.label_col = label_col
@@ -183,6 +192,7 @@ class FineTuningDataset(Dataset) :
         self.unk_idx = special_ids.unk_id
         self.mask_idx = special_ids.mask_id
         self.cls_idx = special_ids.cls_id
+        self.sep_idx = special_ids.sep_id
         self.char_list = list("abcdefghijklmnopqrstuvwxyz0123456789-.")
         self.special_tokens = ['[PAD]', '[UNK]', '[CLS]', '[SEP]', '[MASK]']
         self.all_tokens = self.special_tokens + self.char_list
@@ -204,6 +214,8 @@ class FineTuningDataset(Dataset) :
         elif self.clf_norm == 'pool' :
             ids = token_indices
 
+        ids.append(self.sep_idx)
+
         if len(ids) < self.max_len_c:
             ids += [self.pad_idx] * (self.max_len_c - len(ids))
             
@@ -223,6 +235,8 @@ class FineTuningDataset(Dataset) :
         elif self.clf_norm == 'pool' :
             ids = token_indices
 
+        ids.append(self.sep_idx)
+
         if len(ids) < self.max_len_t:
             ids += [self.pad_idx] * (self.max_len_t - len(ids))
         
@@ -237,3 +251,33 @@ class FineTuningDataset(Dataset) :
         X_char = self.domain_to_ids(domain)
         y = np.int64(label)
         return torch.tensor(X_token, dtype=torch.long), torch.tensor(X_char, dtype=torch.long), torch.tensor(y, dtype=torch.long)
+
+if __name__ == '__main__':
+
+    from transformers import AutoTokenizer
+    import polars as pl
+
+    df = pl.DataFrame({'domain': ['a'], 'label': [1]})
+
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased', use_fast=True)
+    dataset = SubTaskDataset(df, tokenizer=tokenizer, max_len=30, type='subword', tov_norm='cls')
+
+    X_mtp, Y_mtp, X_tpp, Y_tpp, X_tov, Y_tov = dataset[0]
+
+    print(f"\n도메인 원본: {df.row(0)[0]}")
+    print(f"전처리된 X: {X_mtp.shape}")
+    print(f"X MTP(앞 20개): {X_mtp[:20].tolist()}")
+    print(f"X TPP(앞 20개): {X_tpp[:20].tolist()}")
+    print(f'X TOV(앞 20개): {X_tov[:20].tolist()}')
+    print(f'Y MTP(앞 20개): {Y_mtp[:20].tolist()}')
+
+    dataset = FineTuningDataset(df, tokenizer=tokenizer, clf_norm='cls')
+
+    X_token, X_char, y = dataset[0]
+
+    print("도메인 원본:", df.row(0)[0])
+    print("전처리된 X (길이, 토큰):", X_token.shape) 
+    print("전처리된 X (앞 20개, 토큰):", X_token[:20].tolist())
+    print("전처리된 X (길이, 문자):", X_char.shape) 
+    print("전처리된 X (앞 20개, 문자):", X_char[:20].tolist())
+    print("라벨 y:", y.item())
